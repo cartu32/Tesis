cf534faaa28a2e445d1a9e484c46d6456469bb43
esteban <estebancarnuccio@gmail.com>
2025-02-28 20:02:45 -0300

    Revert "-se agrega abumonitor en donde se notifica al samrtwatch que se ingreso area de geofencing"
    This reverts commit c47e39a3f2c6929338684a90d7d04f1071834bb1.

--- Android/Migracion/AbuMonitor/mobile/src/main/java/com/example/comunicationwearmobile/common/Utils.kt
+++ Android/Migracion/AbuMonitor/mobile/src/main/java/com/example/comunicationwearmobile/common/Utils.kt
@@ -5,12 +5,8 @@ import android.content.Intent
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.comunicationwearmobile.models.wearable.WearableDataListenerService
-import com.example.comunicationwearmobile.presenter.notificationWear.helper.NotificationPresenter
 import com.example.shared_library.SharedData
 import com.example.shared_library.toByteArray
-import java.time.LocalDate
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 
 object Utils
 {
@@ -25,28 +21,5 @@ object Utils
             putExtra(SharedData.ParamIntent.MESSAGE_BODY.name, byteArrayData)
         }
         mContext.startService(serviceIntent)
-    }
-
-    fun sendMessageToWearable(mContext: Context,title:String,message:String, typeNotification:SharedData.TypeNotification){
-        val msgNotification:SharedData.MsgNotification
-        val notification= NotificationPresenter.getInstance(mContext)
-
-        var formatter = DateTimeFormatter.ofPattern("HH:mm")
-        val currentTime = LocalTime.now().format(formatter)
-
-        formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-        val currentDay = LocalDate.now().format(formatter)
-
-        msgNotification = SharedData.MsgNotification(
-            title = title,
-            message = message,
-            typeNotification = typeNotification,
-            date = currentDay,
-            hour = currentTime
-        )
-        notification.showNotification(msgNotification)
-        sendMessageToService(mContext ,SharedData.PATH_ADD_NOTIFICATION,msgNotification)
-
-
     }
 }
